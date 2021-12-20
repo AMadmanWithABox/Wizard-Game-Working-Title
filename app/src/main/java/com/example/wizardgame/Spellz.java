051ba28ec0616ef66052a11bd52aa080564d938c
ccolby <christiancolby64@gmail.com>
2021-12-20 15:20:26 -0700

    spell time

---
+++ app/src/main/java/com/example/wizardgame/Spellz.java
@@ -0,0 +1,46 @@
+package com.example.wizardgame;
+
+public class Spellz{
+    //spells
+    spell name;
+}
+enum spell {
+    //story
+    portal, broom, selfillusion, snowball, bookspower, minorillusion, placeholder11, placeholder12, placeholder13, placeholder14, placeholder15, placeholder16,
+    //fire
+    firebolt, fireball, createbonfire, fireshield, crimsonrose, investitureofheat,
+    //ice
+    iceknife, frostbite, investitureofcold, wallofice, icestorm, coneofcold,
+    //oze
+    acidarrow, acidsplash, summonslime, absorbelements, hunger, clone,
+    //water
+    controlwater, splash, rain, bubblerefresher, tsunami, wallofwater,
+    //earth
+    investitureofstone, stoneskin, earthtremor, guardianofstone, wallofstone, stoneprison,
+    //air
+    catapult, controlwinds, investitureofwinds, gust, windstrike, whirlwind,
+    //life
+    barkskin, summonent, enlarge, graspingvine, guardianofnature, thornwhip,
+    //metal
+    investitureofiron, heatmetal, guardianofsteel, spikeshot, summonmagicsword, ironmaiden,
+    //steam
+    blur, investitureoffog, fogcloud, gaseousform, sleep, cloudkill,
+    //poison
+    fear, poisonspray, sickeningradiance, poisoncloud, tipedthornarmor, corruption,
+    //lightning
+    chainlightning, lightningbolt, guardianofthunder, summonstorm, thunderclap, thunderstep,
+    //sound
+    healingword, charm, command, dissonantwhispers, powerwordkill, powerwordstun,
+    //soul
+    astralprojection, deathward, dream, guardianofdream, healingspirit, phantasmalkiller,
+    //light
+    dawn, heal, daylight, investitureofsun, guardianoflight, sunbeam,
+    //dark
+    shadowarms, summonshadow, investitureofmoon, moonbeam, eldritchblast, darkness,
+    //gravity
+    darkstar, forcewave, wallofforce, gravityfissure, magicmissile, magnifygravity,
+    //wild
+    chaosbolt, summonelemental, investitureofchaos, chromaticblast, coinflip, createanti, createvoid, createartha,
+    //null
+    Null
+}
