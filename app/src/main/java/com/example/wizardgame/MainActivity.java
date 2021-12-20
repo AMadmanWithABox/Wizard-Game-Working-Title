bcfe5555585f9184bc26d18b09dd5d5d44061803
ccolby <christiancolby64@gmail.com>
2021-12-20 15:32:32 -0700

    random generator

--- app/src/main/java/com/example/wizardgame/MainActivity.java
+++ app/src/main/java/com/example/wizardgame/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //random number generator. will generate number between min and max.
+    //comment is for commenting into system out so we can see what number
+    //got generated and why.
+    public int Random(int min, int max,String comment) {
+        Random random = new Random();
+        Integer randInt = random.nextInt((max - min) + 1) + min;
+        System.out.println(randInt + " " + comment);
+
+        return randInt;
+    }
+
 
 }
