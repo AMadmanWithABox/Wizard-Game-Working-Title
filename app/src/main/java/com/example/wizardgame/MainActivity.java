9efdaaa01c424bfe95ac55c44667c5656b55f87e
Carter Gamble <carter.gamble014@gmail.com>
2021-12-22 17:15:32 -0700

    Added basic UI

--- app/src/main/java/com/example/wizardgame/MainActivity.java
+++ app/src/main/java/com/example/wizardgame/MainActivity.java
@@ -3,19 +3,32 @@ package com.example.wizardgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ListOfSpells listOfSpells = new ListOfSpells();
+    TextView etInformation;
+    Button bOption1, bOption2, bOption3, bOption4, bStats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+        initUI();
     }
 
+    public void initUI(){
+        etInformation = findViewById(R.id.etInformation);
+        bOption1 = findViewById(R.id.bOption1);
+        bOption2 = findViewById(R.id.bOption2);
+        bOption3 = findViewById(R.id.bOption3);
+        bOption4 = findViewById(R.id.bOption4);
+        bStats = findViewById(R.id.bStats);
+    }
 
     /** random number generator.
      *  generates number between min and max then displays to System.out
